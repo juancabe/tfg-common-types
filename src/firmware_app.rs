effb4f9971f2bca349f1aa971d4db3a929a0962d
Juan Calzada <calzadaj@hotmail.es>
2026-03-03 19:48:32 +0100

    Adapteed Config to crypto module

--- src/firmware_app.rs
+++ src/firmware_app.rs
@@ -1,9 +1,12 @@
 use postcard_bindgen::PostcardBindings;
+const KEY_SIZE: usize = 32;
+const PUB_KEY_SIZE: usize = KEY_SIZE;
+const PRIV_KEY_SIZE: usize = KEY_SIZE;
 
 #[derive(Debug, defmt::Format, serde::Deserialize, serde::Serialize, PostcardBindings)]
 pub enum CommMethod {
     // TODO: placeholder for forwarder's pub key
-    Lora([u8; 10]),
+    Lora([u8; PUB_KEY_SIZE]),
     // SERVER's URL
     Internet([u8; 32]),
 }
@@ -11,8 +14,8 @@ pub enum CommMethod {
 #[derive(Debug, defmt::Format, serde::Deserialize, serde::Serialize, PostcardBindings)]
 pub struct Config {
     // TODO: Create and define the crypto module
-    pub key_pair: [u8; 20],
+    pub secret: [u8; PRIV_KEY_SIZE],
     pub comm_method: CommMethod,
     pub forward_lora: Option<bool>,
-    pub user_id: [u8; 32],
+    pub user_id: [u8; PRIV_KEY_SIZE],
 }
